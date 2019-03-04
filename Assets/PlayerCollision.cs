4b58267a14fe685761280629c52dde472c2cbecd
naggi <nagase2@gmail.com>
2019-03-05 01:04:05 +0900

    update

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision collisionInfo) {
+         Debug.Log(collisionInfo.collider.name);
+        
+    }
+}
