8f77acfeecdaa9246f072c531058604bebe6a095
naggi <nagase2@gmail.com>
2019-03-04 23:41:52 +0900

    first

---
+++ Assets/playerScript.cs
@@ -0,0 +1,26 @@
+﻿
+
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("hello world!");
+        // rb.useGravity = false;
+        //rb.AddForce(0,200,500);
+      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //deltaTimeをかけることで、フレームレートに依存しなくなる。
+        rb.AddForce(0,0,2000 * Time.deltaTime);
+     
+    }
+}
+
