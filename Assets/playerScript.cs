fd14b3304c41d46ed89364f787e505ef9fdb14be
naggi <nagase2@gmail.com>
2019-03-05 00:08:32 +0900

    update

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class playerScript : MonoBehaviour
 {
     public Rigidbody rb;
-  
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +15,24 @@ public class playerScript : MonoBehaviour
         // Debug.Log("hello world!");
         // rb.useGravity = false;
         //rb.AddForce(0,200,500);
-      
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //deltaTimeをかけることで、フレームレートに依存しなくなる。
-        rb.AddForce(0,0,2000 * Time.deltaTime);
-     
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
 
